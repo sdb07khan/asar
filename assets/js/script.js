e235e0ad95b8588aed77d377c5b0fedbc2cb777c
Prince <ksaddab@gmail.com>
2025-05-08 00:09:12 +0530

    home banner partially done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,39 @@
-console.log('hello world');
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", function () {
+  // header hide on scroll down HIGHLIGHT
+  const header = document.querySelector(".header");
+
+  let lastScroll = 0;
+
+  window.addEventListener("scroll", () => {
+    const currentScroll = window.scrollY;
+
+    if (currentScroll > lastScroll && currentScroll > 50) {
+      header.style.top = "-150px";
+    } else if (currentScroll < lastScroll) {
+      header.style.top = "0";
+    }
+
+    lastScroll = currentScroll;
+  });
+
+  // margin top dynamic add HIGHLIGHT
+  const heroElements = document.querySelectorAll(".heroMarginTop");
+
+  function updateHeroMargins() {
+    if (header && heroElements.length > 0) {
+      // Check if elements exist
+      const headerHeight = header.offsetHeight + 50; // +100px extra
+      heroElements.forEach((heroElement) => {
+        heroElement.style.marginTop = `${headerHeight}px`; // Apply to EACH element
+      });
+    }
+  }
+
+  // Initial update
+  updateHeroMargins();
+
+  // Update on resize
+  window.addEventListener("resize", updateHeroMargins);
+});
